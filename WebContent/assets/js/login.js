923799471809dcd7f28d7c2d3bd4a017cd0a5f43
Werner91 <werner91@googlemail.com>
2015-11-05 11:15:14 +0100

    Startbutton

---
+++ WebContent/assets/js/login.js
@@ -0,0 +1,9 @@
+
+function startbutton_disable(){
+	var startButton = document.getElementById("buttonStart"); //Start Button deaktivieren
+	startButton.disabled = true; //Start Button deaktivieren
+}
+
+function addPlayerToTable(){
+	
+}
